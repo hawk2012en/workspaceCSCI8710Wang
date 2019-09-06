f37fce173eaadddc297a4bfd47a8bd7a1925b743
jwang12 <jwang12@chem-its.unl.edu>
2019-09-05 23:53:34 -0500

    Complete implementation of project-ex-0905-message-wang

--- project-ex-0905-message-wang/src/handler/ShowHighestNumberHandler.java
+++ project-ex-0905-message-wang/src/handler/ShowHighestNumberHandler.java
@@ -1,16 +1,38 @@
  
 package handler;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
-
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import util.UtilFile;
 
 
 public class ShowHighestNumberHandler {
 	@Execute
-	public void execute() {
-		List<String> tableContent = UtilFle.readFile("/Users/junwang/Documents/UNO/CSCI8710/ClassExercises/numbers.csv");
+	public void execute(Shell shell) {
+		String basePath = System.getProperty("user.dir");
+		System.out.println(basePath);
+		List<String> contents = UtilFile.readFile("numbers.csv");
+		Collections.sort(contents, Collections.reverseOrder()); 
+		try {
+			UtilFile.saveFile("numbers.txt", contents);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		String output = "Top Five Numbers:";
+		for (int i = 0; i < 5; i++) {
+	         String line = contents.get(i);
+	         if(i == 4)
+	        	 output += " " + line + ".";
+	         else
+	        	 output += " " + line + ",";
+		}
+		
+		MessageDialog.openInformation(shell, "Title", output);
 	}
 		
 }
