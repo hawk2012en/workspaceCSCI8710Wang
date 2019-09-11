8bb4787f5ec505ad8ed82508ac8e64acba835a74
junwang <junwang@10.12.21.207>
2019-09-10 19:59:47 -0500

    Added designpattern5-visitor

---
+++ designpattern5-visitor/src/simpleVisitorPattern/Car.java
@@ -0,0 +1,18 @@
+package simpleVisitorPattern;
+
+import simpleVisitorPattern.part.Body;
+import simpleVisitorPattern.part.Engine;
+import simpleVisitorPattern.part.Wheel;
+import simpleVisitorPattern.visitor.CartPartVisitor;
+
+class Car {
+	Wheel	wheel	= new Wheel("Wheel Part");
+	Body	body	= new Body("Body Part");
+	Engine	engine	= new Engine("Engine Part");
+	
+	public void accept(CartPartVisitor visitor) {
+		wheel.accept(visitor);
+		engine.accept(visitor);
+		body.accept(visitor);
+	}
+}
